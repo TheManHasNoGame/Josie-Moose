07fbd82287ea88702cd373d2cf357b6aebdf5d53
Drew Hartberg <drewhartberg@gmail.com>
2019-09-23 14:13:14 +0000

    Place Create Error

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,8 @@
 class Place < ApplicationRecord
 	belongs_to :user
 	geocoded_by :address
-	after_validation :geocode 
-	
+  	after_validation :geocode
+  	
 	validates :name, presence: true
 	validates :address, presence: true
 	validates :description, length: {minimum: 3}
