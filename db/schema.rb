f840913b45542d00d5aa72042033aee90cfc5ce7
Drew Hartberg <drewhartberg@gmail.com>
2019-09-25 19:55:52 +0000

    AWS Error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_155158) do
+ActiveRecord::Schema.define(version: 2019_09_25_173453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_09_25_155158) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "places", force: :cascade do |t|
